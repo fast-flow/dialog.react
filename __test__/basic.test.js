55fce1da77c31f417cc4af25b25cfff801969852
Nimo <nimo.jser@gmail.com>
2017-02-02 17:53:08 +0800

    0.1.0

--- __test__/basic.test.js
+++ __test__/basic.test.js
@@ -2,7 +2,8 @@ import { Component } from "react"
 import Dialog from "../index"
 import { shallow, mount, render } from "enzyme"
 
-// it('className', function () {
-//     const app = mount(<Dialog className="myclass" />)
-//     expect(app.find('.myclass').length).toEqual(1)
-// })
+it('empty', function () {
+    expect(1).toEqual(1)
+    // const app = mount(<Dialog className="myclass" />)
+    // expect(app.find('.myclass').length).toEqual(1)
+})
